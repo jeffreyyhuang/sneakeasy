4d73ae8343f836226b654bb7d0a356832ff4ab93
Jeffrey Huang <jeffreyhuangcpa@gmail.com>
2021-03-30 12:13:28 -0700

    Designed DBMS schema

---
+++ schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS products;
+
+CREATE DATABASE products;
+
+USE products;
+
+CREATE TABLE sneakers
+(
+  product_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(50) NOT NULL,
+  price INT NOT NULL,
+  brand VARCHAR(30) NOT NULL,
+  release_date DATE NOT NULL,
+  sku VARCHAR(30) NOT NULL,
+  main_color VARCHAR(30) NOT NULL,
+  colorway VARCHAR(50) NOT NULL,
+  designer VARCHAR(30) NOT NULL,
+  silhouette VARCHAR(30) NOT NULL,
+  technology VARCHAR(30) NOT NULL,
+  nickname VARCHAR(30) NOT NULL,
+  category VARCHAR(30) NOT NULL,
+  product_details VARCHAR(3000) NOT NULL,
+  PRIMARY KEY (product_id)
+)
+
+LOAD DATA LOCAL INFILE './SneakerData.csv' INTO TABLE sneakers
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+SET release_date = STR_TO_DATE(@date, '%Y-%m-%d');
