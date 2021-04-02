898d4fb373b9bae05a3b8028a5a7ce7c52564807
Jeffrey Huang <jeffreyhuangcpa@gmail.com>
2021-04-02 02:51:54 -0700

    Implemented main app banner component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React from 'react';
 import styles from '../css_modules/HomePage.module.css';
 import ReleaseGrid from './ReleaseGrid.jsx';
-// import SneakerCard from './SneakerCard.jsx';
+import AppBanner from './AppBanner.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -31,12 +31,17 @@ class App extends React.Component {
    
     return (
       <div className={styles.pageLayoutWrapper}>
-        {/* <div>Hello World</div> */}
+        <AppBanner/>
         <div>
             <div>
-              <div>
-                <div>
+              <div className={styles.moduleSection}>
+                <div className={styles.homeHeroSectionImageWrapper}>
                   <img className={styles.mainImage} alt=" - Air Jordan 5 Retro 'Stealth 2.0'" src="https://cms-cdn.goat.com/750/387013a345f7-2eca-be11-97f8-0ea80d50.jpg" sizes="(max-width: 640px) 200vw, (max-width: 1100px) 150vw, 100vw" class="sc-htpNat ipJcZu HomeHeroSection__Image-slbioc-4 gjdAmw" srcset="https://cms-cdn.goat.com/75/387013a345f7-2eca-be11-97f8-0ea80d50.jpg 75w, https://cms-cdn.goat.com/120/387013a345f7-2eca-be11-97f8-0ea80d50.jpg 120w, https://cms-cdn.goat.com/150/387013a345f7-2eca-be11-97f8-0ea80d50.jpg 150w, https://cms-cdn.goat.com/240/387013a345f7-2eca-be11-97f8-0ea80d50.jpg 240w, https://cms-cdn.goat.com/375/387013a345f7-2eca-be11-97f8-0ea80d50.jpg 375w, https://cms-cdn.goat.com/500/387013a345f7-2eca-be11-97f8-0ea80d50.jpg 500w, https://cms-cdn.goat.com/750/387013a345f7-2eca-be11-97f8-0ea80d50.jpg 750w, https://cms-cdn.goat.com/1000/387013a345f7-2eca-be11-97f8-0ea80d50.jpg 1000w, https://cms-cdn.goat.com/1250/387013a345f7-2eca-be11-97f8-0ea80d50.jpg 1250w, https://cms-cdn.goat.com/1500/387013a345f7-2eca-be11-97f8-0ea80d50.jpg 1500w, https://cms-cdn.goat.com/1800/387013a345f7-2eca-be11-97f8-0ea80d50.jpg 1800w, https://cms-cdn.goat.com/2200/387013a345f7-2eca-be11-97f8-0ea80d50.jpg 2200w, https://cms-cdn.goat.com/2600/387013a345f7-2eca-be11-97f8-0ea80d50.jpg 2600w, https://cms-cdn.goat.com/3000/387013a345f7-2eca-be11-97f8-0ea80d50.jpg 3000w, https://cms-cdn.goat.com/3500/387013a345f7-2eca-be11-97f8-0ea80d50.jpg 3500w"></img>
+                </div>
+                <div className={styles.homeHeroSectionContent}>
+                  <div className={styles.homeHeroTitleTextWrapper}>
+                    <h2 className={styles.homeHeroTitleText}>Air Jordan 5 Retro 'Stealth 2.0'</h2>
+                  </div>
                 </div>
               </div>
             </div>
---
+++ client/components/AppBanner.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from '../css_modules/AppBanner.module.css';
+
+const AppBanner = (props) => {
+  return (
+    <nav className={styles.header}>
+      <div className={styles.headerContent}>
+        <div className={styles.headerStyled}>
+          <div>
+            <div className={styles.hamburgerMenu}>
+              <span className={styles.hamburgerSlice}></span>
+              <span className={styles.hamburgerSlice}></span>
+              <span className={styles.hamburgerSliceFinal}></span>
+            </div>
+          </div>
+        </div>
+        <a className={styles.appLogo}>
+        Sneakeasy
+        </a>
+        <a className={styles.headerDiscover}>Discover</a>
+        <a className={styles.headerShopAll}>Shop</a>
+        <a className={styles.headerTimeline}>Timeline</a>
+        <a className={styles.headerStyles}>Styles</a>
+        <svg className={styles.searchIcon} version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 15 15" xmlSpace="preserve" data-qa="search_icon" class="SearchIcon__TheSearchIcon-sc-1nqz99z-0 kRnXFq" aria-label="search">
+          <g>
+            <g id="HOMEPAGE-HERO-WITH-PLACEHOLDER-FOR-VISUAL---MOBILE-Copy-2" transform="translate(-331.000000, -51.000000)">
+              <g id="Group" transform="translate(338.522463, 58.619673) rotate(-45.000000) translate(-338.522463, -58.619673) translate(330.022463, 48.619673)"><g id="Group-3"><path id="Path-4" d="M8.6,12.9v7" fill="none" stroke-width="1.5"></path><g id="search" transform="translate(8.532978, 8.188775) rotate(-315.000000) translate(-8.532978, -8.188775) translate(3.032978, 2.688775)"><path id="Stroke-4" fill="none" stroke-width="1.5" stroke-linejoin="round" d="M5.2,10.3C2.3,10.3,0,8,0,5.2S2.3,0,5.2,0s5.2,2.3,5.2,5.2S8,10.3,5.2,10.3z"></path></g></g></g>
+            </g>
+          </g>
+        </svg>
+      </div>
+    </nav>
+  )
+}
+
+export default AppBanner;
